b796a4fd2bab9398b8e667f085156a95196a2bf1
Jean-Christophe Pince <jcpince@gmail.com>
2020-10-11 09:56:23 +0200

    Changed Makefile to run offline and deploy.
    Signed-off-by: Jean-Christophe Pince <jcpince@gmail.com>

--- Makefile
+++ Makefile
@@ -2,20 +2,34 @@ MKFILE_PATH := $(abspath $(lastword $(MAKEFILE_LIST)))
 ROOTDIR := $(dir $(MKFILE_PATH))
 
 LOGSDIR := $(ROOTDIR)/logs
+SLS_MODE := "$(SLS_MODE)"
 
 ifeq ($(AUTH0_TOKEN),)
   $(error AUTH0_TOKEN is not set, please source setenv.sh)
 endif
 
-test-offline: test-backend-offline test-frontend-offline
+test: test-backend test-frontend
 
-test-backend-offline:
+test-backend:
 	mkdir -p $(ROOTDIR)/logs
-	cd $(ROOTDIR)/unittests/backend && pytest 2>&1 |tee -a $(LOGSDIR)/test-bakend-offline.log
+	cd $(ROOTDIR)/unittests/backend && pytest 2>&1 |tee -a $(LOGSDIR)/test-bakend-${SLS_MODE}.log
 
-test-frontend-offline:
+test-frontend:
 	@echo "No tests for the frontent yet..."
 
 run-backend-offline:
+ifneq ($(SLS_MODE), "OFFLINE")
+		$(error Backend can only be ran offline, please source setenv.sh offline)
+endif
 	mkdir -p $(ROOTDIR)/logs
-	cd $(ROOTDIR)/backend && sls offline 2>&1 |tee -a $(LOGSDIR)/run-bakend-offline.log
+	cd $(ROOTDIR)/backend && sls offline 2>&1 |tee -a $(LOGSDIR)/run-bakend-${SLS_MODE}.log
+
+run-dynamodb-offline:
+ifneq ($(SLS_MODE), "OFFLINE")
+		$(error DynamoDB can only be ran offline, please source setenv.sh offline)
+endif
+	mkdir -p $(ROOTDIR)/logs
+	cd $(ROOTDIR)/backend && sls dynamodb start --migrate 2>&1 |tee -a $(LOGSDIR)/run-dynamodb-${SLS_MODE}.log
+
+deploy-backend:
+	cd $(ROOTDIR)/backend && sls deploy -v 2>&1 |tee -a $(LOGSDIR)/run-bakend-${SLS_MODE}.log
--- setenv.sh
+++ setenv.sh
@@ -2,10 +2,25 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 TEST_TOKEN='eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlBTcXI2VXFLY3hHUW9xa3lQZTYwNiJ9.eyJpc3MiOiJodHRwczovL2Rldi1zZXkwbTYtcC5ldS5hdXRoMC5jb20vIiwic3ViIjoibjBPVmJpMW5QWXpGRUVmb3pYbFRKM1pIbTc2WEdOTVlAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vZGV2LXNleTBtNi1wLmV1LmF1dGgwLmNvbS9hcGkvdjIvIiwiaWF0IjoxNjAxODA2MTIzLCJleHAiOjE2MDE4OTI1MjMsImF6cCI6Im4wT1ZiaTFuUFl6RkVFZm96WGxUSjNaSG03NlhHTk1ZIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.GbxRtFVzOVjuruZwusuWAof45ZMHlr1b8v7ticDO4pZibksa5bWCuBxugD3NmuYkYWHE-U-zQSphgy6D1Q6DpMqKqvAIDLkfo5QVO2elaOpT0DDYTLqJT_hDeMRDRwqMvbD27yJtSs7nWeGT-XZhF3WHzbbdFBGsN5P8QkMjtNIqvUEc6_xQbw0XSIAFWnpdC5zanLdira-bpMny0HItJ2Yn19xBrf1pYX50pVAKn8934V-HE9UMooc_ELpPjM8e2wb6e17izUX_lsrYHD8uxuJ6VJdai5IPzRm1_T_VojIEfLTOJGg_cGftVeBYrnA6aKGR9jsARCipIhEjYxOwHQ'
 OFFLINE_URL=http://localhost:3000/dev
+AWS_URL=https://nvqtvgyqbh.execute-api.eu-west-3.amazonaws.com/dev
 
-export BASE_URL=$OFFLINE_URL
+is_offline="${1}off"
+
+case $is_offline in
+    "offline" | "Offline" | "off" | "Off" | "OFF")
+        echo "Using offline URL $OFFLINE_URL"
+        export BASE_URL=$OFFLINE_URL
+        export SLS_MODE="OFFLINE"
+        ;;
+    *)
+        echo "Using AWS URL $AWS_URL"
+        export BASE_URL=$AWS_URL
+        export SLS_MODE="ONLINE"
+        ;;
+esac
+
+
 export AUTH0_TOKEN=$TEST_TOKEN
-
 export PYTHONPATH=$BASEDIR/unittests/common:
 
 thispython=`which python`
